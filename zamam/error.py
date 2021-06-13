e135f86ae6e93415166779db9432c0ef957380f2
zamamfarhat <farhatzamam@gmail.com>
2021-06-13 05:59:19 -0700

    Day1 work

---
+++ zamam/error.py
@@ -0,0 +1,10 @@
+ak = "Tell them, tell them all. If anyone comes. I'll kill them, I'll kiil them all"
+
+print(ak)
+
+try:
+  print(x)
+except NameError:
+  print("Variable x is not defined")
+except:
+  print("Something else went wrong")
---
+++ zamam/sorting.py
@@ -0,0 +1,21 @@
+money = [55064578, 55249543, 55972483, 55837292]
+
+money.sort()
+
+print(money)
+
+cgpa = [7.8, 8.00, 7.67, 7.28, 9.01]
+
+cgpa.sort(reverse=True)
+
+print(cgpa)
+
+tv = ["Game", "of", "thrones"]
+
+tv.sort()
+
+print(tv)
+
+jon = ["ccccc","xxxxxx","aaaaaaa","pppppp"]
+print(sorted(jon))
+print(len(jon))
